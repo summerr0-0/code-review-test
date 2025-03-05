24a7363af03b83dd9fde42d524eb5fd681dd0ae5
summerr0-0 <summerr060@gmail.com>
2025-03-05 22:47:43 +0900

    코드리뷰어 테스트

---
+++ src/main/java/org/example/codereviewtest/CodeReviewTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.codereviewtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeReviewTestApplication.class, args);
+    }
+
+}
